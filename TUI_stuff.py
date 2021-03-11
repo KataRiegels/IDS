199bc8db664d6a3b2754bc6b6f3a719b6e07a76e
KataRiegels <KataRiegels@users.noreply.github.com>
2021-03-11 15:53:34 +0100

    how far weve gotten

--- TUI_stuff.py
+++ TUI_stuff.py
@@ -20,13 +20,10 @@ def search(**kwargs):
     response = checkMorePages(response, url_, payload)
     return response
 
-    return payload
-        
 def checkMorePages(response, url_, search_choice):
     size = 50
     pages = 2   
     more_pages = len(response) >= size and pages < 6
-
     while (more_pages):
         url__ = url_  + search_choice + '&page=' + str(pages)
         print(url__)
@@ -37,15 +34,6 @@ def checkMorePages(response, url_, search_choice):
         pages += 1
         size += 50
         more_pages = len(response) >= size and pages < 6
-    return response
-    
-def getAPI(loc):
-    #searching = search(kwargs)
-    searching = search(location = loc)
-    url_ = "https://jobs.github.com/positions.json?"
-    r = requests.get(url = url_ + searching) 
-    response = r.json() 
-    response = checkMorePages(response, url_, searching)
     return response
 
 
@@ -58,22 +46,20 @@ how we worked???
 """
 
 
-
-# then represent it in the npyscreen application in the terminal
 class App(npyscreen.NPSAppManaged):
     def onStart(self):
+        npyscreen.setTheme(npyscreen.Themes.ColorfulTheme)
         #add forms to the application
         self.addForm('MAIN', getInput, name="Enter locations")
+        self.addForm('NO_JOBS', NoJobsForm, name = "Error - no jobs")
         self.addForm('SHOW_JOBS', DisplayJobsForm, name="Job titles")
         self.addForm('JOB_INFO', JobInformationForm, name="info")
 
 class getInput(npyscreen.ActionFormMinimal):
     def create (self):
-        self.add(npyscreen.TitleText, w_id="locationText", name = "Enter a location:" )
+        self.add(npyscreen.TitleText, w_id="locationText", name = "Enter a location:")
         self.add(npyscreen.TitleText, w_id="descriptionText", name = "Enter optional discription:")
    
-        self.returner = []
-        self.jobs_list = []
  
     def afterEditing(self):
         
@@ -86,6 +72,8 @@ class getInput(npyscreen.ActionFormMinimal):
         for resp in self.response:
             self.jobs_list.append(resp)
             self.returner.append(resp['title'])
+        if len(self.jobs_list) < 1:
+            self.parentApp.setNextForm("NO_JOBS")
 
         self.parentApp.getForm('SHOW_JOBS').job.values = self.returner
 
@@ -108,31 +96,29 @@ class DisplayJobsForm(npyscreen.ActionForm):
             self.parentApp.getForm('JOB_INFO').job_title.value = self.chosen_job['title']
             self.parentApp.getForm('JOB_INFO').job_location.value = self.chosen_job['location']
             self.parentApp.getForm('JOB_INFO').job_url.value = self.chosen_job['url']
-        #self.parentApp.getForm('JOB_INFO').job_how_to_apply.value = chosen_job['how_to_apply']
-
     def on_cancel(self):
-        #self.parentApp.switchFormPrevious()
         self.parentApp.setNextForm('MAIN')
-         
-
     def on_ok(self):
         self.parentApp.setNextForm("JOB_INFO")
-        #self.parentApp.switchForm('JOB_INFO')
+
+class NoJobsForm(npyscreen.ActionPopup):
+    def create(self):
+        self.job_company = self.add(npyscreen.FixedText, name = "Error", value = "No jobs found :-( ")
+    def on_ok(self):
+        self.parentApp.setNextForm("MAIN")
+    def on_cancel(self):
+        self.parentApp.setNextForm("MAIN")
+
 
 
 class JobInformationForm(npyscreen.ActionPopupWide):
     def create(self):
-        #self.display = self.add(npyscreen.FixedText, name = "display", value = "Job description for ")
         self.job_company = self.add(npyscreen.TitleText, name = "Company: ")
         self.job_title = self.add(npyscreen.TitleText, name = "Job title: ")
         self.job_location = self.add(npyscreen.TitleText, name = "Location: ")
         self.job_url = self.add(npyscreen.TitleText, name = "Job URL: ")
-        #self.job_how_to_apply = self.add(npyscreen.TitleText, name = "How to apply: ")
-        
     def on_cancel(self):
         self.parentApp.setNextForm("SHOW_JOBS")
-
-
     def on_ok(self):
         self.parentApp.setNextForm(None)
      
