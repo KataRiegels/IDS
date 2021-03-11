44df312bbbf82cbedaa54c8b8b36b733ffaeb156
KataRiegels <KataRiegels@users.noreply.github.com>
2021-03-11 16:03:37 +0100

    latest vers

--- TUI_stuff.py
+++ TUI_stuff.py
@@ -50,21 +50,21 @@ class App(npyscreen.NPSAppManaged):
     def onStart(self):
         npyscreen.setTheme(npyscreen.Themes.ColorfulTheme)
         #add forms to the application
-        self.addForm('MAIN', getInput, name="Enter locations")
-        self.addForm('NO_JOBS', NoJobsForm, name = "Error - no jobs")
-        self.addForm('SHOW_JOBS', DisplayJobsForm, name="Job titles")
-        self.addForm('JOB_INFO', JobInformationForm, name="info")
+        self.addForm('MAIN',      getInput,           name = "Enter locations")
+        self.addForm('NO_JOBS',   NoJobsForm,         name = "Error - no jobs")
+        self.addForm('SHOW_JOBS', DisplayJobsForm,    name = "Job titles")
+        self.addForm('JOB_INFO',  JobInformationForm, name = "info")
 
 class getInput(npyscreen.ActionFormMinimal):
     def create (self):
-        self.add(npyscreen.TitleText, w_id="locationText", name = "Enter a location:")
+        self.add(npyscreen.TitleText, w_id="locationText",    name = "Enter a location:")
         self.add(npyscreen.TitleText, w_id="descriptionText", name = "Enter optional discription:")
    
  
     def afterEditing(self):
         
         self.jobs_list = []
-        self.returner = []
+        self.returner  = []
     
         self.mess1 = self.get_widget("locationText").value
         self.mess2 = self.get_widget("descriptionText").value
@@ -74,7 +74,6 @@ class getInput(npyscreen.ActionFormMinimal):
             self.returner.append(resp['title'])
         if len(self.jobs_list) < 1:
             self.parentApp.setNextForm("NO_JOBS")
-
         self.parentApp.getForm('SHOW_JOBS').job.values = self.returner
 
     
@@ -84,7 +83,7 @@ class getInput(npyscreen.ActionFormMinimal):
 
 class DisplayJobsForm(npyscreen.ActionForm):
     def create (self):
-        self.job = self.add(npyscreen.TitleSelectOne, scroll_exit=True, max_height=11,  name='Jobs')
+        self.job        = self.add(npyscreen.TitleSelectOne, scroll_exit=True, max_height=11,  name='Jobs')
         self.chosen_job =  [] 
         
 
@@ -113,10 +112,10 @@ class NoJobsForm(npyscreen.ActionPopup):
 
 class JobInformationForm(npyscreen.ActionPopupWide):
     def create(self):
-        self.job_company = self.add(npyscreen.TitleText, name = "Company: ")
-        self.job_title = self.add(npyscreen.TitleText, name = "Job title: ")
+        self.job_company  = self.add(npyscreen.TitleText, name = "Company: ")
+        self.job_title    = self.add(npyscreen.TitleText, name = "Job title: ")
         self.job_location = self.add(npyscreen.TitleText, name = "Location: ")
-        self.job_url = self.add(npyscreen.TitleText, name = "Job URL: ")
+        self.job_url      = self.add(npyscreen.TitleText, name = "Job URL: ")
     def on_cancel(self):
         self.parentApp.setNextForm("SHOW_JOBS")
     def on_ok(self):
@@ -131,13 +130,13 @@ app.run()
 
 TO-DO
 - fix naming
-- add art?
-x Widen pop-up 
-- write report
 - Comments
+- make stuff list
+
+
 - Error handling
 
-
+- write report
 
 
 Show: Job title
